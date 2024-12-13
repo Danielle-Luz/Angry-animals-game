1fdc4f872aaacebb2393d6040f06cb3f098feb11
Danielle Luz <daniln2209@gmail.com>
2024-12-13 20:28:13 -0300

    feat: added screen notifier on animal scene

--- scenes/animal/animal.gd
+++ scenes/animal/animal.gd
@@ -2,8 +2,8 @@ extends RigidBody2D
 
 class_name Animal
 
-func _ready() -> void:
-	SignalsAutoload.on_game_ended.connect(die)
+func _physics_process(delta: float) -> void:
+	pass
 
 func die() -> void:
 	queue_free()
