20e9caa743e0c4afe05b30dbc7cb661fd5224d0b
Danielle Luz <daniln2209@gmail.com>
2024-12-14 01:44:00 -0300

    feat: prevent animal from jumping to mouse position when mouse is not over it

--- scenes/animal/animal.gd
+++ scenes/animal/animal.gd
@@ -2,15 +2,25 @@ extends RigidBody2D
 
 class_name Animal
 
+var _is_mouse_over: bool = false
+var _was_mouse_holded: bool = false
+
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("drag"):
+	if Input.is_action_pressed("drag") && (self._is_mouse_over || self._was_mouse_holded):
 		set_animal_position_by_mouse()
-	elif Input.is_action_just_released("drag"):
+	if Input.is_action_just_released("drag") && self._was_mouse_holded:
 		self.freeze = false
 
 func set_animal_position_by_mouse() -> void:
 	self.freeze = true
 	self.position = get_global_mouse_position()
+	self._was_mouse_holded = true
 
 func die() -> void:
 	self.queue_free()
+
+func on_mouse_entered() -> void:
+	self._is_mouse_over = true
+
+func on_mouse_exited() -> void:
+	self._is_mouse_over = false
