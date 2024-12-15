71a26c76c35c612c10acf187aee733511cce9b58
Danielle Luz <daniln2209@gmail.com>
2024-12-15 14:49:00 -0300

    feat: add animal impulse and arrow stretching

--- scenes/animal/animal.gd
+++ scenes/animal/animal.gd
@@ -6,15 +6,19 @@ var _is_mouse_over: bool = false
 var _was_mouse_holded: bool = false
 var _mouse_start_position: Vector2
 var _animal_start_marker: Vector2
+var _arrow_default_scale: float
 
 const _MAX_POSITION: Vector2 = Vector2(0, 60)
 const _MIN_POSITION: Vector2 = Vector2(-60, 0)
+const _IMPULSE_MULTIPLIER: float = 30.0
+const _MAX_ARROW_SCALE: float = 0.435
 
 @onready var _arrow: Sprite2D = $Arrow
 @onready var _arrow_stretch_sound: AudioStreamPlayer2D = $ArrowStretchSound
 
 func _ready() -> void:
-	SignalsAutoload.on_drag_animal.connect(play_arrow_stretch_sound)
+	SignalsAutoload.on_drag_animal.connect(self.play_arrow_stretch_sound)
+	self._arrow_default_scale = self._arrow.scale.x
 
 func _physics_process(delta: float) -> void:
 	var is_animal_moving: bool = self.linear_velocity != Vector2.ZERO
@@ -30,6 +34,10 @@ func _physics_process(delta: float) -> void:
 
 	if Input.is_action_just_released("drag") && self._was_mouse_holded:
 		self.freeze = false
+		self.apply_impulse(self.get_impulse())
+		
+	if is_animal_moving && self._arrow.visible:
+		self._arrow.visible = false
 
 func set_animal_position_by_mouse() -> void:
 	var new_animal_position: Vector2 = get_global_mouse_position()
@@ -50,20 +58,26 @@ func set_animal_position_by_mouse() -> void:
 	
 	self._was_mouse_holded = true
 	self.set_arrow_angle()
-	
+
 func set_arrow_angle() -> void:
-	var distance_from_mouse = self._animal_start_marker - self.position
+	var distance_from_mouse = self.get_animal_moved_distance()
 	distance_from_mouse.x = abs(distance_from_mouse.x)
 	var arrow_angle: float = distance_from_mouse.angle()
 	self._arrow.rotation = arrow_angle
 	
 func play_arrow_stretch_sound() -> void:
-	var is_dragging_animal = self.get_animal_moved_distance() > Vector2.ZERO
+	var distance_from_mouse: Vector2 = self.position - self._mouse_start_position
+	var is_dragging_animal = distance_from_mouse > Vector2.ZERO
 	self._arrow.visible = true
 	
 	if(is_dragging_animal && self._arrow_stretch_sound.playing == false):
 		self._arrow_stretch_sound.position = self.position
 		self._arrow_stretch_sound.play()
+		
+	self.scale_arrow()
+
+func get_impulse() -> Vector2:
+	return self.get_animal_moved_distance() * self._IMPULSE_MULTIPLIER
 
 func die() -> void:
 	self.queue_free()
@@ -74,8 +88,17 @@ func set_mouse_start_position() -> void:
 	self._mouse_start_position = self.global_position if self._was_mouse_holded else get_global_mouse_position()
 	
 func get_animal_moved_distance() -> Vector2:
-	var animal_start_position: Vector2 = self.position
-	return self.position - self._mouse_start_position
+	return self._animal_start_marker - self.position
+
+func scale_arrow() -> void:
+	var max_distance_length: float = (self._MAX_POSITION - self._MIN_POSITION).length()
+	var streched_percentage_from_max_distance: float = self.get_animal_moved_distance().length() / max_distance_length
+	var clamped_arrow_x_scale = clampf(
+		self._arrow_default_scale + streched_percentage_from_max_distance, 
+		self._arrow_default_scale, 
+		self._MAX_ARROW_SCALE
+	)
+	self._arrow.scale.x = clamped_arrow_x_scale
 
 func on_mouse_entered() -> void:
 	self._is_mouse_over = true
