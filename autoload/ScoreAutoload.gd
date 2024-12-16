7f714d528b67cce55373e295b450c967fd7c201d
Danielle Luz <daniln2209@gmail.com>
2024-12-16 00:21:24 -0300

    feat: created store score functionality

--- autoload/ScoreAutoload.gd
+++ autoload/ScoreAutoload.gd
@@ -1,6 +1,29 @@
 extends Node
 
-var score: int = 0
+const SCORE_FILE_PATH: String = "user://score.txt"
+
+var score: Dictionary = { "current_score": 0, "highest_score": 0}
+
+func _ready() -> void:
+	self.score = self.get_score_from_file()
 
 func increment_score() -> void:
-	self.score += 1
+	self.score.current_score += 1
+
+	if(self.score.current_score >= self.score.highest_score):
+		self.score.highest_score = self.score.current_score
+
+func store_score_on_file() -> void:
+	var score_file: FileAccess = FileAccess.open(self.SCORE_FILE_PATH, FileAccess.WRITE)
+	
+	if score_file:
+		score_file.store_string(JSON.stringify(self.score))
+
+func get_score_from_file() -> Dictionary:
+	var score_file: FileAccess = FileAccess.open(self.SCORE_FILE_PATH, FileAccess.READ)
+
+	if score_file && score_file.get_length() > 0:
+		var score_as_json = score_file.get_as_text()
+		return JSON.parse_string(score_as_json)
+
+	return self.score
