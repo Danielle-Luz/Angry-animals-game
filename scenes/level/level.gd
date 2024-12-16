d86b2732d83ef39d1ac145eeadb734e382065f69
Danielle Luz <daniln2209@gmail.com>
2024-12-16 18:45:34 -0300

    feat: added back action

--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -3,10 +3,15 @@ extends Node2D
 @onready var _animal_start_marker: Marker2D = $AnimalStartMarker
 
 const _ANIMAL_SCENE: PackedScene = preload("res://scenes/animal/animal.tscn")
+const _MAIN_SCENE = preload("res://scenes/main/main.tscn")
 
 func _ready() -> void:
 	create_animal_on_start_position()
 	SignalsAutoload.on_game_ended.connect(create_animal_on_start_position)
+	
+func _process(delta: float) -> void:
+	if(Input.is_action_just_pressed("back")):
+		self.navigate_to_main_scene()
 
 func create_animal_on_start_position() -> void:
 	var animal: Animal = _ANIMAL_SCENE.instantiate()
@@ -16,3 +21,6 @@ func create_animal_on_start_position() -> void:
 	
 func store_score_on_file() -> void:
 	ScoreAutoload.store_score_on_file()
+
+func navigate_to_main_scene() -> void:
+	self.get_tree().change_scene_to_packed(self._MAIN_SCENE)
